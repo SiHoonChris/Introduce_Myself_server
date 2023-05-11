2b1d6e60e077cc5511d756f1c282ea68f7875614
SiHoonChris <chrisleegpock@gmail.com>
2023-05-11 22:03:27 +0900

    2023.05.11 About to end

--- routes/mysql.js
+++ routes/mysql.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-  database: "introduce_myself",
-  host: "127.0.0.1",
-  user: "sihoonchris",
-  password: "dltlgns^0426"
+  database: process.env.DB_DATABASE,  
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD 
 });
 
 module.exports = connection;
